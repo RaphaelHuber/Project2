d27a3061ddb556cbb059ef4eb505d2313f7a583c
Raphael Huber <raphael.ehuber@gmail.com>
2019-02-12 10:02:16 -0200

    basic createPokemon layout

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,7 +2,7 @@ function loadSpecies() {
   const selected = document.getElementById('speciesSelect').value;
   axios.get(`http://localhost:3000/loadOneSpecies/${selected}`)
     .then((species) => {
-      console.log('AAAAAA);
+      console.log('AAAAAA');
       // Aqui vem a manipulação de DOM
     });
 }
