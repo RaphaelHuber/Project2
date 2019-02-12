1721ce925c1623714d96b84b49f795505c6ebbfc
Maryssael <guilherme.maryssael@gmail.com>
2019-02-12 10:26:22 -0200

    display

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,7 +2,8 @@ function loadSpecies() {
   const selected = document.getElementById('speciesSelect').value;
   axios.get(`http://localhost:3000/loadOneSpecies/${selected}`)
     .then((species) => {
-      console.log('AAAAAA);
-      // Aqui vem a manipulação de DOM
+      const createPanel = document.getElementById('createPanel');
+      console.log(species.data);
+      createPanel.innerHTML = (`<p>${species.data[0].name}</p><img src="${species.data[0].frontSprite}">`);
     });
 }
