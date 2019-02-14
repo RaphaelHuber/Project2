5b85c2583e89808be6f03506dd066f281ac45bcd
Maryssael <guilherme.maryssael@gmail.com>
2019-02-14 17:51:26 -0200

    movesupdate

---
+++ public/javascripts/data.js
@@ -0,0 +1,17 @@
+const moves = {
+  'Body Slam': {
+    type: 'Normal',
+    power: 85,
+    category: 'Physical'
+  },
+  'Earthquake': {
+    type: 'Ground',
+    power: 100,
+    category: 'Physical'
+  },
+  'Giga Drain': {
+    type: 'Grass',
+    power: 75,
+    category: 'Special'
+  }
+};
